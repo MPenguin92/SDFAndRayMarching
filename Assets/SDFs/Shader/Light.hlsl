765eb4670231b7906053ccbd6ff8ff342499fab9
MPenguin92 <13681162265@163.com>
2024-01-05 12:51:44 +0800

    fix

--- Assets/SDFs/Shader/Light.hlsl
+++ Assets/SDFs/Shader/Light.hlsl
@@ -5,7 +5,7 @@ float4 GetLight(float4 baseColor,float3 normal,float3 lightDir,float3 viewDir,fl
 {
     //漫反射
     float4 diffuse = baseColor * max(0.0f, dot(normal, lightDir)) * intensity;
-    //半程向量,法线和视角向量的和
+    //半程向量,光照方向和视角向量的和
     float3 halfDir = normalize(lightDir + viewDir);
     float4 spec = specular * intensity * pow(saturate(dot(halfDir,normal)),200);
     
